884df08554e897c750b6a7daac2c5833c58b5ad4
caiocarames <119985436+caiocarames@users.noreply.github.com>
2025-02-13 13:20:48 +0000

    teste

---
+++ Projeto/src/Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+    private String marca;
+    private String modelo;
+    private int ano;
+    private double velocidadeAtual;
+
+    public carro (String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+
+    }
+
+}
